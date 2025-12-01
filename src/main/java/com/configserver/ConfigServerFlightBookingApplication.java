a6649e7d6790db6eabe87323039e9ae8860a5ef7
Kanchan Rai <kanchanrai2307@gmail.com>
2025-12-01 20:05:26 +0530

    added: all files

---
+++ src/main/java/com/configserver/ConfigServerFlightBookingApplication.java
@@ -0,0 +1,13 @@
+package com.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerFlightBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerFlightBookingApplication.class, args);
+	}
+
+}
